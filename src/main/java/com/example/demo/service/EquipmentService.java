a6c5619151e2697426721c6f8f8de7f8231b25f7
Saami <uthpala2001@gmail.com>
2024-11-11 11:22:23 +0530

    Equipment service done

--- src/main/java/com/example/demo/service/EquipmentService.java
+++ src/main/java/com/example/demo/service/EquipmentService.java
@@ -7,8 +7,12 @@ import java.util.List;
 
 public interface EquipmentService {
     void saveEquipment(EquipmentDTO equipmentDTO);
+
     List<EquipmentDTO> getAllEquipments();
+
     EquipmentDTO getEquipment(String equipmentId);
+
     void deleteEquipment(String equipmentId);
+
     void updateEquipment(String equipmentId, EquipmentDTO equipmentDTO);
 }
