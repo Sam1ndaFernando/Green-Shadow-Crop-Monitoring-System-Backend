67e5689386287160063d6545d0bd810e5606fef8
Saami <uthpala2001@gmail.com>
2024-11-11 21:53:16 +0530

    Mapping updated

--- src/main/java/com/example/demo/util/Mapping.java
+++ src/main/java/com/example/demo/util/Mapping.java
@@ -15,36 +15,28 @@ public class Mapping {
     private ModelMapper modelMapper;
 
     public CropEntity toCropEntity(CropDTO cropDTO) {
-
         return modelMapper.map(cropDTO, CropEntity.class);
     }
 
-    public CropDTO toCropDto(CropEntity cropEntity) {
-
+    public CropDTO toCropDTO(CropEntity cropEntity) {
         return modelMapper.map(cropEntity, CropDTO.class);
     }
 
-    public List<CropDTO> asCropDtolist(List<CropEntity> cropEntity) {
-        return modelMapper.map(cropEntity, new TypeToken<List<CropDTO>>() {
-        }.getType());
+    public List<CropDTO> asCropDtoList(List<CropEntity> cropEntities) {
+        return modelMapper.map(cropEntities, new TypeToken<List<CropDTO>>() {}.getType());
     }
 
-
     public FieldEntity toFieldEntity(FieldDTO fieldDTO) {
-
         return modelMapper.map(fieldDTO, FieldEntity.class);
     }
 
-    public FieldDTO toFieldDto(FieldEntity fieldEntity) {
-
+    public FieldDTO toFieldDTO(FieldEntity fieldEntity) {
         return modelMapper.map(fieldEntity, FieldDTO.class);
     }
 
     public List<FieldDTO> asFieldDtoList(List<FieldEntity> fieldEntities) {
-        return modelMapper.map(fieldEntities, new TypeToken<List<FieldDTO>>() {
-        }.getType());
+        return modelMapper.map(fieldEntities, new TypeToken<List<FieldDTO>>() {}.getType());
     }
-
 
     public EquipmentEntity toEquipmentEntity(EquipmentDTO equipmentDTO) {
         return modelMapper.map(equipmentDTO, EquipmentEntity.class);
@@ -55,49 +47,42 @@ public class Mapping {
     }
 
     public List<EquipmentDTO> asEquipmentDtoList(List<EquipmentEntity> equipmentEntities) {
-        return modelMapper.map(equipmentEntities, new TypeToken<List<EquipmentDTO>>() {
-        }.getType());
+        return modelMapper.map(equipmentEntities, new TypeToken<List<EquipmentDTO>>() {}.getType());
     }
-
 
     public MonitoringLogEntity toMonitoringLogEntity(MonitoringLogDTO monitoringLogDTO) {
         return modelMapper.map(monitoringLogDTO, MonitoringLogEntity.class);
     }
 
-    public MonitoringLogDTO toMonitoringLogDto(MonitoringLogEntity monitoringLog) {
-        return modelMapper.map(monitoringLog, MonitoringLogDTO.class);
+    public MonitoringLogDTO toMonitoringLogDTO(MonitoringLogEntity monitoringLogEntity) {
+        return modelMapper.map(monitoringLogEntity, MonitoringLogDTO.class);
     }
 
     public List<MonitoringLogDTO> asMonitoringDtoList(List<MonitoringLogEntity> monitoringLogEntities) {
-        return modelMapper.map(monitoringLogEntities, new TypeToken<List<MonitoringLogDTO>>() {
-        }.getType());
+        return modelMapper.map(monitoringLogEntities, new TypeToken<List<MonitoringLogDTO>>() {}.getType());
     }
-
 
     public StaffEntity toStaffEntity(StaffDTO staffDTO) {
         return modelMapper.map(staffDTO, StaffEntity.class);
     }
 
-    public StaffDTO toStaffDto(StaffEntity staffEntity) {
+    public StaffDTO toStaffDTO(StaffEntity staffEntity) {
         return modelMapper.map(staffEntity, StaffDTO.class);
     }
 
-    public List<StaffDTO> asStafDtoList(List<StaffEntity> staffEntities) {
-        return modelMapper.map(staffEntities, new TypeToken<List<StaffDTO>>() {
-        }.getType());
+    public List<StaffDTO> asStaffDtoList(List<StaffEntity> staffEntities) {
+        return modelMapper.map(staffEntities, new TypeToken<List<StaffDTO>>() {}.getType());
     }
-
 
     public VehicleEntity toVehicleEntity(VehicleDTO vehicleDTO) {
         return modelMapper.map(vehicleDTO, VehicleEntity.class);
     }
 
-    public VehicleDTO toVehicleDto(VehicleEntity vehicleEntity) {
+    public VehicleDTO toVehicleDTO(VehicleEntity vehicleEntity) {
         return modelMapper.map(vehicleEntity, VehicleDTO.class);
     }
 
-    public List<VehicleDTO> asVehicleDto(List<VehicleEntity> vehicleEntities) {
-        return modelMapper.map(vehicleEntities, new TypeToken<List<VehicleDTO>>() {
-        }.getType());
+    public List<VehicleDTO> asVehicleDtoList(List<VehicleEntity> vehicleEntities) {
+        return modelMapper.map(vehicleEntities, new TypeToken<List<VehicleDTO>>() {}.getType());
     }
 }
