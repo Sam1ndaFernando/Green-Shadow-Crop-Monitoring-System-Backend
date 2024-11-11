bd38483748b02f35a0865f3cd1c93dfaeabe09cc
Saami <uthpala2001@gmail.com>
2024-11-11 11:01:49 +0530

    FieldService bug fixed

--- src/main/java/com/example/demo/service/FieldService.java
+++ src/main/java/com/example/demo/service/FieldService.java
@@ -1,15 +1,20 @@
 package com.example.demo.service;
 
-import com.example.demo.DTO.FieldStatus;
-import com.example.demo.DTO.IMPL.FieldDTO;
+
+import com.example.demo.dto.FieldStatus;
+import com.example.demo.dto.impl.FieldDTO;
 
 import java.util.List;
 
 public interface FieldService {
     void saveField(FieldDTO fieldDTO);
-    List<FieldDTO>getAllFields();
+
+    List<FieldDTO> getAllFields();
+
     FieldStatus getField(String fieldCode);
+
     void deleteFields(String fieldCode);
+
     void updateField(String fieldCode, FieldDTO fieldDTO);
 
 }
