f642a60987581dec560d5717355f0fea3430dfc5
Saami <uthpala2001@gmail.com>
2024-11-11 10:59:43 +0530

    EquipmentService done

---
+++ src/main/java/com/example/demo/service/EquipmentService.java
@@ -0,0 +1,14 @@
+package com.example.demo.service;
+
+
+import com.example.demo.dto.impl.EquipmentDTO;
+
+import java.util.List;
+
+public interface EquipmentService {
+    void saveEquipment(EquipmentDTO equipmentDTO);
+    List<EquipmentDTO> getAllEquipments();
+    EquipmentDTO getEquipment(String equipmentId);
+    void deleteEquipment(String equipmentId);
+    void updateEquipment(String equipmentId, EquipmentDTO equipmentDTO);
+}
