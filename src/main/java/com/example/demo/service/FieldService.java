6ac9d6e30c5a98023c9cb66c6c9f45fdf6e3f497
Saami <uthpala2001@gmail.com>
2024-11-11 11:00:00 +0530

    FieldService done

---
+++ src/main/java/com/example/demo/service/FieldService.java
@@ -0,0 +1,15 @@
+package com.example.demo.service;
+
+import com.example.demo.DTO.FieldStatus;
+import com.example.demo.DTO.IMPL.FieldDTO;
+
+import java.util.List;
+
+public interface FieldService {
+    void saveField(FieldDTO fieldDTO);
+    List<FieldDTO>getAllFields();
+    FieldStatus getField(String fieldCode);
+    void deleteFields(String fieldCode);
+    void updateField(String fieldCode, FieldDTO fieldDTO);
+
+}
